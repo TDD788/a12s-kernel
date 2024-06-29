24f8ff98a3754833f1054b6cba9ab3fc4188198d
Azmathulla Shaik <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-06-29 21:42:43 +0530

    Add : toolchain export variables

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-export CROSS_COMPILE=<android platform directory you download>/android/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-
-export CROSS_COMPILE=/usr/local/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-                        
-export CC=<android platform directory you download>/android/prebuilts/clang/host/linux-x86/clang-r353983c/bin/clang
-
-export CC=/usr/local/toolchain/clang/host/linux-x86/clang-r353983c/bin/clang
-                        
-export CLANG_TRIPLE=<android platform directory you download>/android/prebuilts/clang/host/linux-x86/clang-r353983c/bin/aarch64-linux-gnu-
-
-export CLANG_TRIPLE=/usr/local/toolchain/clang/host/linux-x86/clang-r353983c/bin/aarch64-linux-gnu-
+export ARCH": "arm64",
+      "CC": "clang"
+        "CLANG_TRIPLE": "aarch64-linux-gnu-",
+        "CROSS_COMPILE": "aarch64-linux-android-",
+        "CROSS_COMPILE_ARM32": "arm-linux-androideabi-",
+        "LD": "ld.lld",
+        "AR": "llvm-ar",
+        "NM": "llvm-nm",
+        "OBJCOPY": "llvm-objcopy",
+        "OBJDUMP": "llvm-objdump",
+        "READELF": "llvm-readelf",
+        "OBJSIZE": "llvm-size",
+        "STRIP": "llvm-strip",
+        "LDGOLD": "aarch64-linux-gnu-ld.gold",
+        "LLVM_AR": "llvm-ar",
+        "LLVM_DIS": "llvm-dis",
+        "CONFIG_THINLTO": ""-
  
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
